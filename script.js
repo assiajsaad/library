f0bac4f11199f340d5f5e8de54695e40ae4f0931
assiajsaad <assia.j.saad@gmail.com>
2024-03-05 01:37:40 +0200

    Added cards display. Made form functional

--- script.js
+++ script.js
@@ -1,21 +1,72 @@
 const myLibrary = [];
 
-
-function Book(title,author,pages,isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, Read : ${this.isRead}`
-    }
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, Read : ${this.isRead}`;
+  };
 }
 
-function addBookToLibrary(){
-    
+function showBooks() {
+    let content = document.querySelector('.content');
+    let card = document.createElement('div');
+    card.className = 'card';
+    let cardText = document.createElement('div');
+    cardText.className = 'card-text';
+    let delBtn = document.createElement('button');
+    delBtn.className = 'btn-delete';
+    delBtn.textContent = "Delete Item";
+
+  myLibrary.map((book) => {
+    cardText.textContent = book.info();
+    card.appendChild(cardText)
+    card.appendChild(delBtn);
+    content.appendChild(card);
+  });
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+  let book = new Book(
+    title,
+    author,
+    pages,
+    isRead
+  );
+
+  myLibrary.push(book);
 }
 
 
-let book = new Book('Freakonomics','Author','230',true);
+let form = document.querySelector("#bookForm");
 
-console.log(book.info());
+form.addEventListener('submit',(event)=>{
+    let bookTitle = document.querySelector('#bookTitle').value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let isRead = document.querySelector('input[name="isRead"]:checked').value;
+    addBookToLibrary(bookTitle, author, pages, isRead);
+    showBooks();
+    event.preventDefault();
+});
+
+
+
+
+
+
+
+
+
+
+
+addBookToLibrary(
+  "Freakonomics",
+  "Stephen J. Dubner and Steven Levit",
+  230,
+  true
+);
+
+showBooks();
