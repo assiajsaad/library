e7adb8dd41e881e1d5cdeeaaf586d80fc0be0237
assiajsaad <assia.j.saad@gmail.com>
2024-03-05 18:01:47 +0200

    Added toggle isRead button

--- script.js
+++ script.js
@@ -11,72 +11,93 @@ function Book(title, author, pages, isRead) {
 }
 
 function showBooks() {
-    let content = document.querySelector('.content');
-    let card = document.createElement('div');
-    card.className = 'card';
-    let cardText = document.createElement('div');
-    cardText.className = 'card-text';
-    let delBtn = document.createElement('button');
-    delBtn.className = 'btn-delete';
-    delBtn.textContent = "Delete Item";
+  let content = document.querySelector(".content");
+  let card = document.createElement("div");
+  card.className = "card";
+  let cardText = document.createElement("div");
+  cardText.className = "card-text";
+  let delBtn = document.createElement("button");
+  delBtn.className = "btn-delete";
+  delBtn.textContent = "Delete Item";
 
-    
-    
-    
+  let btnToggleRead = document.createElement("button");
+  btnToggleRead.className = "btn-toggle-read";
+  btnToggleRead.textContent = "Change Read Status";
 
   myLibrary.map((book, index) => {
     cardText.textContent = book.info();
-    
-    card.appendChild(cardText)
+
+    card.appendChild(cardText);
+
+    card.appendChild(btnToggleRead);
     card.appendChild(delBtn);
     content.appendChild(card);
-    delBtn.addEventListener('click',(index)=>{
+
+    // content.addEventListener("click", (event) => {
+    //   if (event.target.classList.contains(".btn-delete")) {
+    //     myLibrary.splice(index, 1);
+    //     delBtn.parentElement.remove();
+    //   } else if (event.target.classList.contains(".btn-toggle-read")) {
+    //     book.isRead = !book.isRead;
+    //   }
+    // });
+
+    delBtn.addEventListener('click',()=>{
         myLibrary.splice(index,1);
-        console.log(delBtn.parentElement.parentElement);
-        content.removeChild(delBtn.parentElement);
-        // showBooks();
+        delBtn.parentElement.remove();
+
+    })
+    btnToggleRead.addEventListener('click',(event)=>{
+      const cardIndex = Array.from(content.children).indexOf(event.target.parentElement);
+      if(myLibrary[cardIndex].isRead === "Yes"){
+        myLibrary[cardIndex].isRead = 'No';
+      }else if (myLibrary[cardIndex].isRead === "No");{
+        myLibrary[cardIndex].isRead = 'Yes';
+      }
+      console.log(myLibrary[cardIndex].isRead);
+      // showBooks();
     })
   });
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
-  let book = new Book(
-    title,
-    author,
-    pages,
-    isRead
-  );
+  let book = new Book(title, author, pages, isRead);
 
   myLibrary.push(book);
 }
 
-
 let form = document.querySelector("#bookForm");
 
-form.addEventListener('submit',(event)=>{
-    let bookTitle = document.querySelector('#bookTitle').value;
-    let author = document.querySelector("#author").value;
-    let pages = document.querySelector("#pages").value;
-    let isRead = document.querySelector('input[name="isRead"]:checked').value;
-    addBookToLibrary(bookTitle, author, pages, isRead);
-    showBooks();
-    event.preventDefault();
+form.addEventListener("submit", (event) => {
+  let bookTitle = document.querySelector("#bookTitle").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let isRead = document.querySelector('input[name="isRead"]:checked').value;
+  addBookToLibrary(bookTitle, author, pages, isRead);
+  showBooks();
+  event.preventDefault();
 });
 
+// let cardContainer = document.querySelector('.container');
+//   cardContainer.addEventListener('click', (event) => {
+//   if (event.target.classList.contains('delete-btn')) {
+//     const card = event.target.parentNode;
+//     const index = Array.from(cardContainer.children).indexOf(card);
+//     if (index !== -1) {
+//       cardsData.splice(index, 1);
+//       card.remove();
+//     }
+//   } else if(event.target.classList.contains('btn-toggle-read')) {
 
-
-
-
-
-
+//   }
+// });
 
 addBookToLibrary(
   "Freakonomics",
   "Stephen J. Dubner and Steven Levit",
   230,
-  true
+  'Yes'
 );
 
 showBooks();
-
-
+console.log(myLibrary);
